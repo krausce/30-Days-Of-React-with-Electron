397580db32e8b77fa9c0e4ad8169cff172d6ec3c
krausce <ckraus200982@gmail.com>
2020-12-16 16:59:03 +0200

    04 - initial setup completed 03

--- 03_Day_Setting_Up/first_react_app/src/App.js
+++ 03_Day_Setting_Up/first_react_app/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import frontEndTech from './images/frontend_technologies.png'
+import avatar from './images/Chritopher Kraus_linkedin.jpg'
 import './App.css';
+import { useState } from "react";
+
+const subscribed = [];
+const techSkills = ['HTML', 'CSS', 'Sass', 'JS', 'React', 'Node', 'MongoDB', 'Python', 'Flask', 'Django', 'NumPy', 'Pandas', 'Data' +
+' Analysis', 'MYSQL', 'GraphQL', 'D3.js', 'Gatsby', 'Docker', 'Heroku', 'Git'];
+
+class subscriber {
+    constructor(props) {
+        this.firstName = props.firstName;
+        this.lastName = props.lastName;
+        this.email = props.email;
+        this._props = props;
+    }
+
+    toString() {
+        return 'Name: ' + this.firstName + ' ' + this.lastName + '\n' + 'Email: ' + this.email;
+    }
+
+    get props() {
+        return this._props;
+    }
+}
+
+function Subscribe () {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    return (
+        <form>
+            <h1 align={'center'}>SUBSCRIBE</h1>
+            <div className={'App-subscribe'}>
+                <input type={'text'} className={'input-text'} value={firstName} placeholder={'First Name'}
+                       onChange={event => setFirstName(event.target.value)} />
+                <input type={'text'} className={'input-text'} value={lastName} placeholder={'Last Name'}
+                       onChange={event => setLastName(event.target.value)} />
+                <input type={'text'} className={'input-text'} value={email} placeholder={'Email'}
+                       onChange={event => setEmail(event.target.value)} />
+            </div>
+            <button className="Submit" onClick={() => {
+                subscribed.push(new subscriber({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email
+                }));
+                setFirstName('');
+                setLastName('');
+                setEmail('');
+                alert(subscribed.map(s => s.toString()));
+            }} >Submit</button>
+        </form>
+    );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {<img src={frontEndTech} className="App-frontend" alt="tech" />}
+          {Subscribe()}
       </header>
+        <body className="App-body">
+            {<img src={avatar} className="App-avatar" alt="avatar"/>}
+            {techSkills.map(s => {
+                return (
+                    <div className="App-skill">
+                        <p>{s}</p>
+                    </div>
+                )
+            })}
+        </body>
     </div>
   );
 }
---
+++ 04_Day_Components/second_react_app/src/electron-starter.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {} {
+        createWindow();
+    }
+});
